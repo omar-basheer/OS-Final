4fd0fd3574a221568b6dd396563a48d34f2a70de
selomcaleb <selomcaleb@gmail.com>
2024-03-24 20:46:02 +0000

    adding shortest time remaining scheduling algorithm

--- algorithms.c
+++ algorithms.c
@@ -60,7 +60,52 @@ void priority_scheduler(struct Queue* queue){
 
 
 void shortestTimeRemaining(struct Queue* queue){
-    
+    if(isEmpty(queue)){
+        printf("Queue is empty. Cannot schedule.\n");
+        return;
+    };
+
+    struct Queue* tempQueue = initQueue();
+    struct Process* currentProcess = NULL;
+    int totalProcessses = queue ->size;
+    int currentTime = 0;
+
+    while(totalProcessses > 0){
+        while(!isEmpty(queue) && queue->head->process->arrival_time <= currentTime){
+            currentProcess = dequeue(queue);
+            enqueue(tempQueue, currentProcess);
+        }
+
+        struct QueueNode* tempNode = tempQueue->head;
+        struct Process* shortestRemainingProcess = tempNode->process;
+        while (tempNode != NULL){
+            if(tempNode->process->remaining_time < shortestRemainingProcess->remaining_time){
+                shortestRemainingProcess = tempNode->process;
+            }
+            tempNode = tempNode->next;
+        }
+
+        if(shortestRemainingProcess != NULL){
+            int remainingTime = shortestRemainingProcess->remaining_time - 1;
+            currentTime++;
+            if(remainingTime == 0){
+                shortestRemainingProcess->completion_time = currentTime;
+                calculateTurnaroundTime(shortestRemainingProcess);
+                calculateWaitingTime(shortestRemainingProcess, currentTime);
+                totalProcessses--;
+            }
+            else{
+                shortestRemainingProcess->remaining_time = remainingTime;
+                enqueue(tempQueue, shortestRemainingProcess);
+            }
+        }
+
+        else{
+            currentTime++;
+        }
+    }
+
+
 }
 
 
